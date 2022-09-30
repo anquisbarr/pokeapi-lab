a9b8cf61d72322aa08ad4208361b53361855e0b2
JoseChavezLTS <jose.chavez@ntgfreight.com>
2022-09-29 21:30:52 -0500

    feat: up

--- index.js
+++ index.js
@@ -1,23 +1,82 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 const axios = require('axios');
 
-var URL = "https://pokeapi.co/api/v2/pokemon/"
+var URL = 'https://pokeapi.co/api/v2/pokemon/';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+ async function sendLoginEmail() {
 
-app.get('/query',(req, res) => {
-let name = req.query.name
-axios.get(`${URL}${name}`).then((apiRes) => {
-  console.log(apiRes.data.types)
-  res.send(200,{response: {tipo : apiRes.data.types, habilidades:  apiRes.data.abilities}})
-})
-})
+  const emailData = {
+    personalizations: [
+      {
+        to: [
+          {
+            email: "cbalbuena@utec.edu.pe",
+          },
+        ],
+        subject: "Group 2, server error, 500",
+      },
+    ],
+    from: {
+      email: "sebastian.quispe.b@utec.edu.pe",
+    },
+    content: [
+      {
+        type: "text/html",
+        value: `<span>hubo un error en el server del grupo 2</span>`,
+      },
+    ],
+  };
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  const options = {
+    method: "POST",
+    url: "https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send",
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": "3a7915be9dmsh3eb18fc16ff0937p1a074cjsn29a26fb6341b",
+      "X-RapidAPI-Host": "rapidprod-sendgrid-v1.p.rapidapi.com",
+    },
+    data: emailData,
+  };
+
+  console.log(emailData);
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
+}
+
+app.get('/', (req, res) => {});
+
+app.get('/query', (req, res, next) => {
+  try {
+    let name = req.query.name;
+    axios
+      .get(`${URL}${name}`)
+      .then((apiRes) => {
+        res.send(200, {
+          response: {
+            tipo: apiRes.data.types,
+            habilidades: apiRes.data.abilities,
+          },
+        });
+      })
+      .error((error) => {
+        sendLoginEmail().then(res=>console.log("correo enviado"))
+        next(error)
+      });
+  } catch (error) {
+    sendLoginEmail().then(res=>console.log("correo enviado"))
+    next(error)
+  }
+});
+
+app.listen(port, () => {});
